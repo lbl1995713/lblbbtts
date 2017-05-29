2831d003b354fb08359f67ad907fce3de1930ba5
liaobolin <liaobolin@hzwitwin.com>
2017-05-29 21:35:12 +0800

    lbl
    我的毕业设计

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
+  // required to lint *.vue files
   plugins: [
     'html'
   ],
